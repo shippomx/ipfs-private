cd7cee887e3d2181dae104e1a514c685837705ce
MiaoXia <flysommer@outlook.com>
2020-08-05 17:54:32 +0800

    <feat>一个简单的搭建ipfs私链的脚本

---
+++ init.sh
@@ -0,0 +1,45 @@
+mkdir logs nodes
+
+home=$PWD
+
+function initNode() {
+    cd $home
+    nodeDir=$home/nodes/$1/
+    rm -rf $nodeDir && mkdir $nodeDir
+    cp $home/swarm.key $nodeDir
+    ipfs -config=$nodeDir init 
+    ipfs -config=$nodeDir bootstrap rm --all
+    # 替换端口
+    sed -ig "s/tcp\/4001/tcp\/400$2/" $nodeDir/config # Swarm
+    sed -ig "s/tcp\/5001/tcp\/500$2/" $nodeDir/config # API
+    sed -ig "s/tcp\/8080/tcp\/600$2/" $nodeDir/config # gateway
+    rm -rf $nodeDir/configg
+}
+
+
+function connectMainNode() {
+    mainNode=$home/nodes/$1
+    key=$(sed -n 89p $mainNode/config)
+    head=${key#*\"PeerID\"\: \"}
+    PeerID=${head%\"\,}
+    nohup ipfs -config=$mainNode daemon > $home/logs/$1.log 2>&1 &
+    for(( i=1;i<${#nodes[@]};i++)) do
+        ipfs -config=$home/nodes/${nodes[i]} bootstrap add /ip4/127.0.0.1/tcp/4001/ipfs/$PeerID
+        nohup ipfs -config=$home/nodes/${nodes[i]} daemon > $home/logs/${nodes[i]}.log 2>&1 &
+    done
+}
+
+nodes=("magic" "manage" "node1" "node2")
+
+go run $home/main.go > $home/swarm.key
+i=1
+for node in ${nodes[@]}
+do
+    initNode "$node" $i
+    let i+=1
+done
+
+echo "\n======== 初始化节点完成 ===========\n"
+
+connectMainNode $nodes
+rm $home/swarm.key
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"log"
+)
+
+func main() {
+	key := make([]byte, 32)
+	_, err := rand.Read(key)
+	if err != nil {
+		log.Fatalln("While trying to read random source:", err)
+	}
+
+	fmt.Println("/key/swarm/psk/1.0.0/")
+	fmt.Println("/base16/")
+	fmt.Print(hex.EncodeToString(key))
+}

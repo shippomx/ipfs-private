dd1ea7c732fc0c5f4028d81d74d0f80c00f37bdb
MiaoXia <flysommer@outlook.com>
2020-08-07 15:28:20 +0800

    <fix>通过jq命令进行解析, 保证稳定性

--- init.sh
+++ init.sh
@@ -22,10 +22,9 @@ function initNode() {
 
 function connectMainNode() {
     mainNode=$home/nodes/$1
-
-    key=$(cat $mainNode/config | awk -F "[PeerID]" '/PeerID/{print$0}')
-    head=${key#*\"PeerID\"\: \"}
-    PeerID=${head%\"\,}
+    value=$(cat $mainNode/config | jq '.Identity.PeerID')
+    head=${value#*\"}
+    PeerID=${head%\"}
     nohup ipfs -config=$mainNode daemon > $home/logs/$1.log 2>&1 &
     for(( i=1;i<${#nodes[@]};i++)) do
         ipfs -config=$home/nodes/${nodes[i]} bootstrap add /ip4/127.0.0.1/tcp/4001/ipfs/$PeerID
